2972a3aa6c0a341ddc97984b128e58ef55e3b3b4
Ruslan <rgimaev1984@gmail.com>
2022-01-28 17:20:25 +0500

    added main program code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from itertools import permutations
 
 points = {
     'point1': [0, 2],
@@ -6,6 +7,7 @@ points = {
     'point3': [5, 2],
     'point4': [6, 6],
     'point5': [8, 3]}
+
 start = 'point1'
 
 
@@ -15,7 +17,9 @@ def dist(a, b):
 
 
 def distances_matrix(points_cords):
+
     dm = {}
+
     for point1, cords1 in points_cords.items():
         dm[point1] = {}
         for point2, cords2 in points_cords.items():
@@ -24,3 +28,27 @@ def distances_matrix(points_cords):
 
 
 if __name__ == '__main__':
+
+    min_path_length = 0
+    min_path = ()
+    dm = distances_matrix(points)
+    transitional_points = points.copy()
+    transitional_points.pop(start)
+    transitional_paths = permutations(transitional_points.keys(), len(transitional_points))
+
+    for path in transitional_paths:
+        path = (start,) + path + (start,)
+        path_length = 0
+        i = 0
+        while i < len(path) - 1:
+            point1 = path[i]
+            point2 = path[i + 1]
+            path_length += dm[point1][point2]
+            i += 1
+        if min_path_length == 0 or path_length < min_path_length:
+            min_path_length = path_length
+            min_path = path
+
+    print(min_path_length)
+    print(min_path)
+
